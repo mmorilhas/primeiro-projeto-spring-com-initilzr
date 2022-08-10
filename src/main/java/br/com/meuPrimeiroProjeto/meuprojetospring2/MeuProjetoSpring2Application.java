6e6beaba58b9d3dd968cb9b0ce6f15dcce3f3786
Michelle Morilhas <michelle.morilhas@gmail.com>
2022-08-09 22:52:33 -0300

    initial commit

---
+++ src/main/java/br/com/meuPrimeiroProjeto/meuprojetospring2/MeuProjetoSpring2Application.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojetospring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuPrimeiroProjeto/meuprojetospring2/MeuProjetoSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojetospring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
